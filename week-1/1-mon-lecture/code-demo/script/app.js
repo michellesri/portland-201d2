fa4151cc3c20882de00fabcd679383c9692a574a
a-she <al@alswebdesigns.net>
2016-03-09 18:49:14 -0800

    Interactive prompts

--- week-1/1-mon-lecture/code-demo/script/app.js
+++ week-1/1-mon-lecture/code-demo/script/app.js
@@ -1,5 +1,17 @@
-var leftPocket  = 8.85;
-var rightPocket = 3.49;
+var total   = document.getElementById('total');
+var inLeft  = document.getElementById('inLeft');
+var inRight = document.getElementById('inRight');
+
+/*var leftPocket  = 8.85;
+  var rightPocket = 3.49;
+*/
+leftString  = prompt("Enter amount of change in your left  pocket");
+leftPocket  = parseFloat( leftString);
+inLeft.textContent  = 'Your left  pocket has $'+leftPocket +' in change';
+
+rightString = prompt("Enter amount of change in your RIGHT pocket");
+rightPocket = parseFloat(rightString);
+inRight.textContent = 'Your right pocket has $'+rightPocket +' in change';
 
 function add(a, b) {
     var c = a + b;
@@ -7,6 +19,4 @@ function add(a, b) {
 }
 
 var totalChange = add(leftPocket, rightPocket);
-
-var output = document.getElementById('output');
-output.textContent = 'totalChange = $' + totalChange;
+total.textContent = 'Total pocket change: $' + totalChange;
