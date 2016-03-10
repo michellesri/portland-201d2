b830bfc4606562a07ce568f07e8d6f90c1cbde06
a-she <al@alswebdesigns.net>
2016-03-10 00:19:07 -0800

    Added conditional styling

--- week-1/1-mon-lecture/code-demo/script/app.js
+++ week-1/1-mon-lecture/code-demo/script/app.js
@@ -6,12 +6,16 @@ var inRight = document.getElementById('inRight');
   var rightPocket = 3.49;
 */
 leftString  = prompt("Enter amount of change in your left  pocket");
+//var p = confirm("Blah?");
+//console.log('p = '+p);
 leftPocket  = parseFloat( leftString);
 inLeft.textContent  = 'Your left  pocket has $'+leftPocket +' in change';
+console.log('Your left  pocket has $'+leftPocket +' in change');
 
 rightString = prompt("Enter amount of change in your RIGHT pocket");
 rightPocket = parseFloat(rightString);
 inRight.textContent = 'Your right pocket has $'+rightPocket +' in change';
+console.log('Your right pocket has $'+rightPocket +' in change');
 
 function add(a, b) {
     var c = a + b;
@@ -20,3 +24,10 @@ function add(a, b) {
 
 var totalChange = add(leftPocket, rightPocket);
 total.textContent = 'Total pocket change: $' + totalChange;
+console.log('Total pocket change: $' + totalChange);
+
+if (totalChange > 1000000) {
+  total.textContent += ' <-- Wow, that\'s a lot of pocket change!';
+  total.style = 'color:red';
+  console.log(' <-- Wow, that\'s a lot of pocket change!');
+}
