1df262ed0ea370ba240c1e858c8cc098bfd6b043
a-she <al@alswebdesigns.net>
2016-03-15 15:31:05 -0700

    fixed margins changed by emacs

--- week-1/3-tue-lecture/code-demo/app.js
+++ week-1/3-tue-lecture/code-demo/app.js
@@ -9,17 +9,17 @@ alert('Hey there ' + userName + ', I\'m going to ask you some questions about me
 var keepAsking = true;
 
 while (keepAsking) {
-    var userResponse = prompt('Was, I born on the moon?');
-    console.log('userResponse: ' + userResponse);
+  var userResponse = prompt('Was, I born on the moon?');
+  console.log('userResponse: ' + userResponse);
 
-    if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
-        alert('Nope, I was actualy born on the moon. I\'ma moonian computer language.');
-        keepAsking = false;
-    } else if (userResponse === 'Y') { // TODO: Make this more robust!
-        alert('Nice, I was born on the moon');
-        keepAsking = false;
-    } else {
-        alert('Ooops!, you should have given a Y or N, so you loose this round.');
-    }
+  if (userResponse.toLowerCase() === 'n' || userResponse.toUpperCase() === 'NO') {
+    alert('Nope, I was actualy born on the moon. I\'ma moonian computer language.');
+    keepAsking = false;
+  } else if (userResponse === 'Y') { // TODO: Make this more robust!
+    alert('Nice, I was born on the moon');
+    keepAsking = false;
+  } else {
+    alert('Ooops!, you should have given a Y or N, so you loose this round.');
+  }
 
 } // while
