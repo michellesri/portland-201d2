5786a58e43bce1bc40aafc8b99112e226fba6ccc
a-she <al@alswebdesigns.net>
2016-03-17 10:33:03 -0700

    WIP, added jshing

--- week-1/9-fri-lecture/code-demo/gulpfile.js
+++ week-1/9-fri-lecture/code-demo/gulpfile.js
@@ -1,5 +1,8 @@
 var gulp    = require('gulp');
 var mocha   = require('gulp-mocha');
+var gulp_jshint = require('gulp-jshint');
+
+var js_src   = ['./*.js', './js/*.js', './test/*.js'];
 
 gulp.task('watch-mocha', function() {
     gulp.watch(['test/**', 'index.js'], ['watch-mocha']);
@@ -9,4 +12,10 @@ gulp.task('watch-mocha', function() {
         .pipe(mocha({
             reporter: 'list'
         }));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(js_src)
+        .pipe(gulp_jshint())
+        .pipe(gulp_jshint.reporter('default'));
 });
