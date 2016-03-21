87a2e5967911dac4c6f640d6d2f9a20828b27786
a-she <al@alswebdesigns.net>
2016-03-21 02:21:22 -0700

    Simpler assert form

--- week-1/9-fri-lecture/code-demo/gulp/test/test.js
+++ week-1/9-fri-lecture/code-demo/gulp/test/test.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var app = require('../js/app.js');
 
-describe('my first test', function() {
+describe('My first test', function() {
   it('should check first question', function() {
     assert.deepEqual(app.getQuestion(0), 'first');
   });
@@ -10,15 +10,7 @@ describe('my first test', function() {
     assert.deepEqual(app.getQuestion(2), 'third');
   });
 
-  it('Should increment 0 to get 5', function() {
-    assert.equal(app.addFive(0), 5);
-  });
-
-  it('Should increment 4 to get 9', function() {
-    assert.equal(app.addFive(4), 9);
-  });
-
-  it('Should increment -1008 to get -1003', function() {
-    assert.equal(app.addFive(-1008), -1003);
-  });
+  it('should increment 0 to get 5', function() { assert(5 == app.addFive(0)); });
+  it('should increment 4 to get 9', function() { assert(9 == app.addFive(4)); });
+  it('should increment -1008 to get -1003', function() { assert(app.addFive(-1008) == -1003); });
 });
