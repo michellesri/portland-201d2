21cd80ea980b41507d8930e7917195cfad5f91d2
a-she <al@alswebdesigns.net>
2016-03-21 02:27:13 -0700

    app.js moved to compute.js to make 'room' for browser-specific app.js

--- week-1/9-fri-lecture/code-demo/gulp/test/test.js
+++ week-1/9-fri-lecture/code-demo/gulp/test/test.js
@@ -1,16 +1,16 @@
 var assert = require('assert');
-var app = require('../js/app.js');
+var compute = require('../js/compute.js');
 
 describe('My first test', function() {
   it('should check first question', function() {
-    assert.deepEqual(app.getQuestion(0), 'first');
+    assert.deepEqual(compute.getQuestion(0), 'first');
   });
 
   it('should check third question', function() {
-    assert.deepEqual(app.getQuestion(2), 'third');
+    assert.deepEqual(compute.getQuestion(2), 'third');
   });
 
-  it('should increment 0 to get 5', function() { assert(5 == app.addFive(0)); });
-  it('should increment 4 to get 9', function() { assert(9 == app.addFive(4)); });
-  it('should increment -1008 to get -1003', function() { assert(app.addFive(-1008) == -1003); });
+  it('should increment 0 to get 5', function() { assert(5 == compute.addFive(0)); });
+  it('should increment 4 to get 9', function() { assert(9 == compute.addFive(4)); });
+  it('should increment -1008 to get -1003', function() { assert(compute.addFive(-1008) == -1003); });
 });
