b797156013b694a2206592c09bd09e693765b351
a-she <al@alswebdesigns.net>
2016-03-21 02:21:01 -0700

    Removed unused code

--- week-1/9-fri-lecture/code-demo/gulp/gulpfile.js
+++ week-1/9-fri-lecture/code-demo/gulp/gulpfile.js
@@ -5,32 +5,20 @@ var jshint = require('gulp-jshint');
 var js_src   = ['./*.js', './js/*.js', './test/*.js'];
 var html_src = './*.html';
 
-gulp.task('watch-mocha', function() {
-    gulp.watch(['test/**', 'index.js'], ['watch-mocha']);
-    return gulp.src(['test/*.js'], {
-            read: false
-        })
-        .pipe(mocha({
-            reporter: 'list'
-        }));
-});
-
 gulp.task('lint', function() {
-    return gulp.src(js_src)
+  return gulp.src(js_src)
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('test', function() {
-    return gulp.src('./test/test.js', {read: false})
+  return gulp.src('./test/test.js', {read: false})
         .pipe(mocha({reporter: 'spec'}));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(js_src,   ['lint', 'test']);
-    gulp.watch(html_src, ['test']);
+  gulp.watch(js_src,   ['lint', 'test']);
+  gulp.watch(html_src, ['test']);
 });
 
 gulp.task('default', ['lint', 'test', 'watch']);
-
-
