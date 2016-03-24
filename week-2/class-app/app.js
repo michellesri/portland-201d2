aaec9bcb249f3438703a113a67e664355c8bd288
a-she <al@alswebdesigns.net>
2016-03-24 15:21:40 -0700

    Form added

--- week-2/class-app/app.js
+++ week-2/class-app/app.js
@@ -15,3 +15,24 @@ addLi ('listTwo','giraffe');
 var removeEl = document.getElementById('pRemove');
 var containerEl = removeEl.parentNode;
 containerEl.removeChild(removeEl);
+
+var clickTheButton = document.getElementById("cuteLittleButton");
+
+//eventListener and function for click event to add to ul
+clickTheButton.addEventListener("click", function() {
+  var userInput = document.getElementById("textBox").value;
+  addLi("myList", userInput);
+}
+);
+
+//blur is opposite of focus (tab away)
+clickTheButton.addEventListener("blur", function() {
+  var userInput = document.getElementById("textBox").value;
+  addLi('listTwo', userInput);
+}
+);
+
+//blur is opposite of focus (tab away)
+clickTheButton.addEventListener('focus', function() {
+  console.log('focus event occured');
+});
