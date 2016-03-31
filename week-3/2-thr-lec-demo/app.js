f52484dcf6bcd3f090a420ebff5af418efa9c119
a-she <al@alswebdesigns.net>
2016-03-31 00:15:29 -0700

    + simple canvas/context test

--- week-3/2-thr-lec-demo/app.js
+++ week-3/2-thr-lec-demo/app.js
@@ -14,3 +14,13 @@ txt.addEventListener('input', function() {
   localStorage['txt'] = v;
   out.textContent     = v;
 });
+
+// Canvas context test
+var c = gebi('c');
+
+var ctx = c.getContext('2d');
+console.log('ctx = ' + ctx);
+
+var ctx2 = c.getContext('2d');
+console.log('ctx2 = ' + ctx2);
+
