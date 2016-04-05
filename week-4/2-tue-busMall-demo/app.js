1c930246a33dbe83e8b1d0a0501e26efe433aaf6
a-she <al@alswebdesigns.net>
2016-04-05 10:54:06 -0700

    Re-arranged code sections, rev'd comments

--- week-4/2-tue-busMall-demo/app.js
+++ week-4/2-tue-busMall-demo/app.js
@@ -1,4 +1,35 @@
-//===== DOM & localStorage related to DOM state =====
+//===== MODEL: the app's data =====
+var voteThresh1 = 16;
+var voteThresh2 =  8 + voteThresh1;
+var names = ['Holm', 'Tate', 'Rousey', 'Jones',
+             'McGregor', 'Rockhold', 'Winslow', 'McCarthy'];
+var nameMap  = [], fighters = [];
+var roundClicks, blockVote;
+
+function fighter() { // Leave names in separate array (less mem)
+  this.nDisp = 0;
+  this.nSel = 0;
+}
+
+function makeFighters() {
+  for (var ii = 0; ii < names.length; ii++) {
+    fighters.push(new fighter());
+    nameMap.push([ii, names[ii]]);
+  }
+}
+
+function setBlockVote(b)   { blockVote   = b; localStorage.blockVote   = blockVote;   }
+function setRoundClicks(n) { roundClicks = n; localStorage.roundClicks = roundClicks; }
+
+//===== VIEW: DOM & localStorage related to DOM state =====
+var viz = ['hidden', 'visible'];
+var domIm = [gebi('im1'), gebi('im2'), gebi('im3')];
+var btnShow = gebi('show');
+var btnMore = gebi('more');
+var btnNew  = gebi('new');
+var chart   = gebi('chart');
+var caps = [gebi('cap1'), gebi('cap2'), gebi('cap3')];
+
 function gebi(id) { return document.getElementById(id); }
 
 function setBtnViz(a, b, c) {
@@ -9,14 +40,6 @@ function setBtnViz(a, b, c) {
   localStorage.btnMore = b;
   localStorage.btnNew = c;
 }
-
-var viz = ['hidden', 'visible'];
-var domIm = [gebi('im1'), gebi('im2'), gebi('im3')];
-var btnShow = gebi('show');
-var btnMore = gebi('more');
-var btnNew  = gebi('new');
-var chart   = gebi('chart');
-var caps = [gebi('cap1'), gebi('cap2'), gebi('cap3')];
 
 im1.addEventListener('click', vote);
 im2.addEventListener('click', vote);
@@ -37,40 +60,11 @@ btnNew.addEventListener('click', function() { // New voting round
   setBlockVote(false);
 });
 
-function show() { // Show vote results
-  dump();
-  setBtnViz(0, 0, 1);
-  roundClicks = 0;
-  localStorage.roundClicks = roundClicks;
-}
-
-//===== Data Model =====
-var voteThresh1 = 16;
-var voteThresh2 =  8 + voteThresh1;
-var names = ['Holm', 'Tate', 'Rousey', 'Jones',
-             'McGregor', 'Rockhold', 'Winslow', 'McCarthy'];
-var nameMap  = [], fighters = [];
-var roundClicks, blockVote;
-
-function fighter() { // Leave names in separate array (less mem)
-  this.nDisp = 0;
-  this.nSel = 0;
-}
-
-function makeFighters() {
-  for (var ii = 0; ii < names.length; ii++) {
-    fighters.push(new fighter());
-    nameMap.push([ii, names[ii]]);
-  }
-}
-
-//===== View logic ======
+//===== CONTROLLER: Logic that updates/shows data in the view ======
 function loadImg(pool, ide, cap) { // "ide" = image DOM element
   var pIdx = Math.floor(Math.random() * pool.length);
-  ide.setAttribute('src', 'img/' + pool[pIdx][1] + '.jpg');
-  cap.textContent = pool[pIdx][1];
   var idx = pool[pIdx][0];
-  ide.idx = idx;
+  initIde(ide, idx, cap);
   fighters[idx].nDisp++;
   localStorage.stats = JSON.stringify(fighters);
   pool.splice(pIdx, 1); // Wed's "+1 EC": splice as imgs are shown
@@ -78,21 +72,22 @@ function loadImg(pool, ide, cap) { // "ide" = image DOM element
 
 function loadImages() {
   nmTmp = nameMap.slice();
-    for (var ii = 0; ii < domIm.length; ii++) loadImg(nmTmp, domIm[ii], caps[ii]);
+  for (var ii = 0; ii < domIm.length; ii++)  loadImg(nmTmp, domIm[ii], caps[ii]);
   localStorage.idx1 = im1.idx;
   localStorage.idx2 = im2.idx;
   localStorage.idx3 = im3.idx;
 }
 
-function setBlockVote(b) {
-  blockVote = b;
-  localStorage.blockVote = blockVote;
+function show() { // Show vote results
+  dump();
+  setBtnViz(0, 0, 1);
+  setRoundClicks(0);
 }
 
 function vote(ev) {
   if (blockVote) return;
 
-  roundClicks++; localStorage.roundClicks = roundClicks;
+  setRoundClicks(roundClicks+1);
   fighters[ev.target.idx].nSel++;
   localStorage.stats = JSON.stringify(fighters);
 
@@ -110,23 +105,23 @@ function vote(ev) {
 function dump() { // Print vote stats
   chart.innerHTML = '';
   for (var ii = 0; ii < fighters.length; ii++) {
-    var v = fighters[ii].nSel; // votes
-    var s = fighters[ii].nDisp; // shown
-    var p = 0; // percent
-    if (v) p = Math.round(100 * v/s, 1);
+    var v = fighters[ii].nSel;  // Vote count
+    var s = fighters[ii].nDisp; // Display count
+    var p = 0; // Percent
+    if (v)  p = Math.round(100 * v/s, 1); // p=0 if never displayed
     chart.innerHTML += names[ii] + ': ' + v + ' votes, ' + s + ' shows (' + p + '%)<br>';
   }
   localStorage.chart = true;
 }
 
-//===== Init. Load from localStorage if available
+  // === Init. Load from localStorage if available
 function initIde(ide, lsIdx, cap) {
   ide.idx = lsIdx;
   ide.setAttribute('src', 'img/' + nameMap[ide.idx][1] + '.jpg');
   cap.textContent = names[lsIdx];
 }
 
-(function() { // This is an IIFE
+(function() { // This is an IIFE!
   makeFighters();
   if (typeof localStorage.btnShow != 'undefined') {
     setBtnViz(localStorage.btnShow, localStorage.btnMore, localStorage.btnNew);
@@ -137,16 +132,14 @@ function initIde(ide, lsIdx, cap) {
     initIde(im2, localStorage.idx2, cap2);
     initIde(im3, localStorage.idx3, cap3);
 
-    if (typeof localStorage.stats != 'undefined')
-      fighters = JSON.parse(localStorage.stats);
-
-    if (localStorage.chart != 'false') { show(); }
+    if (typeof localStorage.stats != 'undefined')  fighters = JSON.parse(localStorage.stats);
+    if (localStorage.chart != 'false')             show();
   }
   else {
     setBtnViz(0,0,0);
     setBlockVote(false);
     localStorage.chart = false;
-    roundClicks = 0; localStorage.roundClicks = roundClicks;
+    setRoundClicks(0);
     loadImages();
   }
 })();
